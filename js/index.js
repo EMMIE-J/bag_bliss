bf5a80267a489c0ef8a5921ba120e7c23193aaac
emmie-j <attachiegrace408@gmail.com>
2024-06-10 10:00:41 +0000

    first commit

---
+++ js/index.js
@@ -0,0 +1,25 @@
+
+
+const btn = document.getElementById('btn-btn')
+btn.onclick = function(){
+    alert('Wait, more products coming soon')
+}
+
+const btn2 = document.getElementById('btn-btn')
+btn2.onclick = function(){
+    alert('Crafted for perfection')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
